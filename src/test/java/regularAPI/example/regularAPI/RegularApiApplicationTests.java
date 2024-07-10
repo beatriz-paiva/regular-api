7928e2eb0307e55a354d9702f3b939470578155b
beatriz-paiva <beatriz.paiva.rb@gmail.com>
2024-07-10 16:05:00 -0300

    initializer

---
+++ src/test/java/regularAPI/example/regularAPI/RegularApiApplicationTests.java
@@ -0,0 +1,13 @@
+package regularAPI.example.regularAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegularApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
